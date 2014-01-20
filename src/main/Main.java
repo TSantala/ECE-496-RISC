5ca2f8137fbf03c12d1f53a5cd84fd81c1595178
JohnGodbey <jrg35@duke.edu>
2014-01-20 18:37:15 -0500

    removed the failed experiment

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,28 +1,9 @@
 package main;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
-import gameElements.GameClient;
-import gameElements.GameServer;
-import gameElements.UmbraRoom;
-
 public class Main {
-	private final static int UMBRA_PORT = 30480;
 
 	public static void main(String[] args){
-		System.out.println("Wahoo!");
 		// new RiscGame();
-		(new Thread(new UmbraRoom())).start();
-		try {
-			ServerSocket ss=new ServerSocket(UMBRA_PORT);
-			Socket s = ss.accept();
-			new GameClient(new Socket());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			System.out.println("shit, done fucked up");
-		}
 	}
 
 }
