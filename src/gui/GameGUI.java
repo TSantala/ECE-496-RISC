2f25ab12a8400ee4c30f4717ccc3386ee8e5b853
TSantala <timo.santala@duke.edu>
2014-02-02 00:37:54 -0500

    Push GUI skeleton

---
+++ src/gui/GameGUI.java
@@ -0,0 +1,18 @@
+package gui;
+
+import java.applet.Applet;
+
+public class GameGUI extends Applet {
+	
+	public GameGUI(){
+		System.out.println("GameGUI constructor");
+	}
+
+	public void init(){
+		
+		System.out.println("Game initialization!");
+		
+	}
+	
+	
+}
