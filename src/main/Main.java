ee9bfeb01ab1260311854a0bce3ab1c195f51e67
TSantala <timo.santala@duke.edu>
2014-02-05 13:38:05 -0500

    Merge in main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,5 @@
 package main;
 
-<<<<<<< HEAD
 //import javax.swing.SwingUtilities;
 //import gameElements.Initialization;
 import gui.GameGUI;
@@ -8,16 +7,6 @@ import server.ObjectClient;
 import server.ObjectServer;
 //import server.Server;
 //import server.SocketClient;
-=======
-import javax.swing.SwingUtilities;
-
-import gameElements.Initialization;
-import server.ObjectClient;
-import server.ObjectServer;
-import server.Server;
-//import gui.GameGUI;
-import server.SocketClient;
->>>>>>> a00132ffcb4c5bf42fd7261edf697a98005dc2e4
 
 public class Main {
 
@@ -25,13 +14,8 @@ public class Main {
 		//Initialization i = new Initialization();
 		//SwingUtilities.invokeLater(i);
 		//new Server();
-<<<<<<< HEAD
-
-		new GameGUI();
-=======
 		
 //		new GameGUI();
->>>>>>> a00132ffcb4c5bf42fd7261edf697a98005dc2e4
 
 		/*Server myServer = new Server();
 		myServer.start();
