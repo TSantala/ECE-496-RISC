f5111e5322a866e77f680e89c07b285d7d3e347f
JohnGodbey <jrg35@duke.edu>
2014-02-05 13:37:24 -0500

    fixed the ip

--- src/server/ObjectClient.java
+++ src/server/ObjectClient.java
@@ -18,7 +18,7 @@ public class ObjectClient extends Thread implements ServerConstants{
 		System.out.println("ObjectClient initialized");
 		try {
 
-			InetAddress address = InetAddress.getByName("10.190.7.36");
+			InetAddress address = InetAddress.getByName("10.190.84.134");
 			System.out.println("Address is: "+InetAddress.getLocalHost().getHostAddress());
 
 			Socket connection = new Socket(address, port);
