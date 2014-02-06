286735e6c50825ab9c43ed28f229cb553184be55
JohnGodbey <jrg35@duke.edu>
2014-02-05 21:34:02 -0500

    I so hope I dont mess everyone up with this one

--- src/gameElements/Initialization.java
+++ src/gameElements/Initialization.java
@@ -8,73 +8,56 @@ import java.util.*;
 
 import javax.swing.*;
 
+import server.Message;
+import server.ObjectClient;
+
 /*
  * This is the class that is called when the game begins- has to be passed number of players 
  */
 public class Initialization implements Runnable
 {
-	private String userInput;
+	private JTextField input;
+	private JTextArea output;
+	private ObjectClient myClient;
+
+	public Initialization(ObjectClient client){
+		myClient = client;
+	}
+
 	@Override
 	public void run() {
-		JFrame f = new JFrame("Initialization phase");
-		f.setSize(600, 400);
+		JFrame f = new JFrame("RISC");
+		f.setSize(1000, 800);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setLayout(new BorderLayout());
 		Container pane = f.getContentPane();
-		
-		final JTextField output = new JTextField(40);
-		output.setText("INITIALIZING RIGHT NOW");
-		
-		final JTextField input = new JTextField(40);
-		
-		JButton b1 = new JButton("Add stuff here");
-		b1.addActionListener(new ActionListener() {
+
+		output = new JTextArea(5, 40);
+		JScrollPane scrollingOutput = new JScrollPane(output); 
+		output.setEditable(false);
+
+		input = new JTextField(40);
+		input.addActionListener(new ActionListener(){
+
 			public void actionPerformed(ActionEvent e) {
-				userInput = input.getText();
-				output.setText(userInput);
-			}
+				myClient.sendMessage(new Message(input.getText()));
+				input.setText("");
+			}			
 		});
-		
-//		String s = (String)JOptionPane.showInputDialog(
-//                f,
-//                "Please enter # of players:\n"
-//                ,
-//                JOptionPane.PLAIN_MESSAGE,
-//                icon,
-//                null,
-//                "ham");
-//
-//		//If a string was returned, say so.
-//		if ((s != null) && (s.length() > 0)) {
-//			setLabel("Green eggs and... " + s + "!");
-//			return;
-//		}
-		
+
+		JPanel b1 = new JPanel(); //will become visual component
+
 		pane.add(b1,BorderLayout.PAGE_START);
+		pane.add(scrollingOutput, BorderLayout.CENTER);
 		pane.add(input,BorderLayout.PAGE_END);
-		pane.add(output, BorderLayout.CENTER);
-	
+
 		f.pack();
 		f.setVisible(true);
-		
+
 	}
-//	public Initialization() //just trying to get something down...
-//	{
-//		int numPlayers = 2;
-//		int numStartingUnits = 3;
-//		int numTerritories = 2;
-//		GameMap map = new GameMap();
-//		List<Territory> tlist = new ArrayList<Territory>();
-//		Group g1 = new Group();
-//		Group g2 = new Group();
-//		for (int i = 0; i < numPlayers*numTerritories; i++) //want groups to be evenly distributed
-//		{
-//			Territory temp = new Territory();
-//			tlist.add(temp);
-//			if (i==0 || i == 2)
-//				g1.addTerritory(temp);
-//		}
-//		
-//	}
+	
+	public void printMessage(String s){
+		output.setText(output.getText() + "\n" + s);
+	}
 
 }
--- src/server/ObjectSocket.java
+++ src/server/ObjectSocket.java
@@ -9,33 +9,33 @@ public class ObjectSocket extends Thread implements ServerConstants{
 	//private String myTimeStamp;
 	private ObjectServer myServer;
 	//private int myID;
-    private ObjectOutputStream oos ;
-    private ObjectInputStream ois;
-    private String id;
-    
+	private ObjectOutputStream oos ;
+	private ObjectInputStream ois;
+	private String id;
+
 	public ObjectSocket(Socket socket, int id, ObjectServer server) {
 		myConnection = socket;
 		//myID = id;
 		myServer = server;
 		try {
-		ois = new ObjectInputStream(myConnection.getInputStream());
-		oos = new ObjectOutputStream(myConnection.getOutputStream());
+			ois = new ObjectInputStream(myConnection.getInputStream());
+			oos = new ObjectOutputStream(myConnection.getOutputStream());
 		} catch (IOException e){
 			System.out.println("Initializing the Object Socket Server input and output streams failed.");
 		}
-		
+
 	}
-	
-    public synchronized void sendMessage(Message m) {
-    	try {
+
+	public synchronized void sendMessage(Message m) {
+		try {
 			oos.writeObject(m);
 			oos.flush();
 		} catch (IOException e) {
 			System.out.println("Object output stream broke in the thread.");
 		}
-        
-    }
-	
+
+	}
+
 	@Override
 	public void run() {
 		while(true){
@@ -43,17 +43,19 @@ public class ObjectSocket extends Thread implements ServerConstants{
 				Message m = (Message)ois.readObject();
 				String process = m.getMessage();
 				String[] input = process.split(" ");
-				if (input[0].equals("name"))
+				if (input[0].equals("name")){
 					id = input[1];
+					continue;
+				}
 				if (process.equals("exit")){
 					myConnection.close();
 					System.out.println("Server is closing the connection " + id);
 					myServer.removeConnection(this);
 					break;
 				}
-					
+
 				System.out.println(process);
-				
+
 				myServer.broadCastMessage(new Message(id + ": " +m.getMessage()));
 
 			}
