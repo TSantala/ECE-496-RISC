194bb54b19cc4e145b8c95beeedebaaa5385026c
JohnGodbey <jrg35@duke.edu>
2014-03-02 21:05:01 -0500

    added cleaner way to do initial unit placement

--- src/gameElements/GameModel.java
+++ src/gameElements/GameModel.java
@@ -255,11 +255,7 @@ public class GameModel implements ServerConstants {
 	}
 	
 	private void feedUnits(){
-		for(Player p : myGame.getPlayers()){
-			for(Unit u : p.getUnits()){
-				
-			}
-		}
+		
 	}
 	
 	private void harvestTerritories(){
