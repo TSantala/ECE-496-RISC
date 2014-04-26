40fe81e8734b73a49f70fd0ca521ae7e81a47b26
Vincent Wang <vw22@duke.edu>
2014-04-26 08:50:02 -0400

    Small change to troop movement for alliances

--- src/gameElements/GameModel.java
+++ src/gameElements/GameModel.java
@@ -217,8 +217,7 @@ public class GameModel implements ServerConstants, Serializable {
 				return;
 			}
 		}
-
-		else if(myGame.getMap().hasPath(from,to,p))
+		else if(myGame.getMap().hasPath(from,to,p) && to.getOwner().onTeam(from.getOwner()))
 		{
 			from.removeUnits(units);
 			to.addUnits(units);
@@ -310,8 +309,10 @@ public class GameModel implements ServerConstants, Serializable {
 
 	public List<Command> checkValidAttacks(List<Command> cl){
 		List<Command> toReturn = new ArrayList<Command>();
-		for(Command c : cl){
-			if(!myGame.getMap().canAttack(c.getFrom(),c.getTo(),c.getPlayer())){
+		for(Command c : cl)
+		{
+			if(!myGame.getMap().canAttack(c.getFrom(),c.getTo(),c.getPlayer()))
+			{
 				//this.redoTurnErrorFound("Invalid attack!");
 				this.broadcastGameMessage("Player "+c.getPlayer().getName()+" has committed an invalid attack. Ignoring command.");
 			}
