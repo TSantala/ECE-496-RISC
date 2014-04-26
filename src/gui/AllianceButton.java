583b865d3121b09dadc5b1deecd8364f152e9c91
TSantala <timo.santala@duke.edu>
2014-04-26 06:44:01 -0400

    fully sync up

---
+++ src/gui/AllianceButton.java
@@ -0,0 +1,42 @@
+package gui;
+/*
+ * Very similar to Attack button...refactor soon
+ */
+import gameElements.MoveCommand;
+import gameElements.Territory;
+import gameElements.Unit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class AllianceButton extends JButton 
+{
+	public AllianceButton(final GameGUI gui)
+	{
+	    super("Request Alliance");
+	    this.addActionListener(new ActionListener()
+	    {
+	        public void actionPerformed (ActionEvent arg0) 
+	        {
+            	//Create and set up the window.
+            	JFrame frame = new JFrame("Attack");
+            	frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+            	//Create and set up the content pane.
+            	JComponent newContentPane = new AttackDialog(gui.getLeftClickUnits(),gui,frame,"Attack!");
+            	newContentPane.setOpaque(true); //content panes must be opaque
+            	frame.setContentPane(newContentPane);
+
+            	//Display the window.
+            	frame.pack();
+            	frame.setVisible(true);
+		}
+	    });
+	}
+
+}
